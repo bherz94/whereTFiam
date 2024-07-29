109bae688f702fc0f415224dd33684a37dc01dee
Benedikt Herz <benedikt.herz@aracom.de>
2024-07-29 07:22:48 +0200

    Initial commit

---
+++ src/client/java/dfgben/wheretfiam/WhereTFiamClient.java
@@ -0,0 +1,10 @@
+package dfgben.wheretfiam;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WhereTFiamClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
